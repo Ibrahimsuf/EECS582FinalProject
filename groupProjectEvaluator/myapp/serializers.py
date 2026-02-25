4a14bbc0c2e00623c2f2091c57ab69207adad7e7
AbhiroopGoel <157642305+AbhiroopGoel@users.noreply.github.com>
2026-02-24 20:33:06 -0600

    Update serializers.py

--- groupProjectEvaluator/myapp/serializers.py
+++ groupProjectEvaluator/myapp/serializers.py
@@ -1,64 +1,48 @@
 from rest_framework import serializers
 from .models import Task, Sprint, Member, Project, Group
 
-# Description: Serializer classes for database models for data conversion
-# Programmer(s): Abhriroop Goel, Dylan Kneidel, Ian Lim, Kit Magar, Bryce Martin, Ibrahim Sufi 
-# Created: 2026-2-25
-# Revised: 2026-2-25
 
 class SprintSerializer(serializers.ModelSerializer):
     class Meta:
         model = Sprint
-        fields = '__all__'
+        fields = "__all__"
+
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ['id', 'title', 'description', 'status', 'sprint', 'created_at', 'member']
+        fields = ["id", "title", "description", "status", "sprint", "created_at", "member"]
 
-from rest_framework import serializers
-from .models import Task, Sprint, Member, Project, Group
 
-class SprintSerializer(serializers.ModelSerializer):
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Sprint
-        fields = '__all__'
+        model = Group
+        fields = "__all__"
 
-class TaskSerializer(serializers.ModelSerializer):
+
+class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Task
-        fields = ['id', 'title', 'description', 'status', 'sprint', 'created_at', 'member']
+        model = Project
+        fields = "__all__"
+
 
 class MemberSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = Member
         fields = [
             "id",
-            "name", "first_name", "last_name",
-            "email", "username",
+            "name",
+            "first_name",
+            "last_name",
+            "email",
+            "username",
+            "password",
             "roles",
-            "university", "address", "photo",
-            "group", "project",
-            "password",  # keep for now since your app uses plain password
+            "university",
+            "address",
+            "photo",
+            "group",
+            "project",
         ]
-
-class ProjectsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = '__all__'
-
-class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Group
-        fields = '__all__'
-
-class ProjectsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Project
-        fields = '__all__'
-
-class GroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Group
-        fields = '__all__'
-
