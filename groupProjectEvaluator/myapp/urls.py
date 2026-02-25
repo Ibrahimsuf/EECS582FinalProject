e7729e1f58521d9a05a9470fa85eb1694e5d4f99
AbhiroopGoel <157642305+AbhiroopGoel@users.noreply.github.com>
2026-02-24 20:33:37 -0600

    Add GroupViewSet and ProjectViewSet to URLs

--- groupProjectEvaluator/myapp/urls.py
+++ groupProjectEvaluator/myapp/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, SprintViewSet, MemberViewSet, register, login
+
+from .views import (
+    TaskViewSet,
+    SprintViewSet,
+    MemberViewSet,
+    GroupViewSet,
+    ProjectViewSet,
+    register,
+    login,
+)
 
 router = DefaultRouter()
 router.register(r"tasks", TaskViewSet)
 router.register(r"sprints", SprintViewSet)
 router.register(r"members", MemberViewSet)
+router.register(r"groups", GroupViewSet)
+router.register(r"projects", ProjectViewSet)
 
 urlpatterns = [
     path("auth/register/", register),
