7f376c733e29a30d807de04d86c5a125b4776acc
AbhiroopGoel <157642305+AbhiroopGoel@users.noreply.github.com>
2026-02-24 19:57:49 -0600

    Add allowed hosts and CORS settings in settings.py

--- groupProjectEvaluator/groupProjectEvaluator/settings.py
+++ groupProjectEvaluator/groupProjectEvaluator/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
